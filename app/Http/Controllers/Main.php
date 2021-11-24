167a11f233284a6755ac6661e674338fe716cf23
ABDALAZARD <vinicius.abdala@audeedu.com.br>
2021-11-24 10:49:14 -0300

    ATUALIZA MAIN E ROUTE

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -4,65 +4,84 @@ namespace App\Http\Controllers;
 
 use App\Models\Player;
 use App\Models\Time;
-use Illuminate\Http\Request;
+
+
+
 
 class Main extends Controller
 {
 
 
-    public function registroClube(){
+    public function registroclube(){
 
         $novo_time = new Time;
+        $novo_time->time_id = 6;
+        $novo_time->nome_time = "Manchester United";
 
-        $novo_time->time_id = 2;
-        $novo_time->nome_time = "Flamengo";
+        if($novo_time->save()){
 
-        $novo_time->save();
-
-        return redirect()->route("home");
-
+            return redirect()->route('home');
+        }
+        else{
+            echo "!ok";
+        }
     }
 
 
-    public function registroJogador(){
+    public function registrojogador(){
 
         $novo_jogador = new Player;
 
-        $novo_jogador->time_id = 2;
-        $novo_jogador->nome_jogador = "Gustavo Mosquito";
-        $novo_jogador->posicao = "PTE";
-        $novo_jogador->numero = 19;
+        $novo_jogador->time_id = 5;
+        $novo_jogador->nome_jogador = "Virgil Van Dijk";
+        $novo_jogador->posicao = "ZC";
+        $novo_jogador->numero = 4;
         $novo_jogador->save();
 
-        return redirect()->route("home");
+        if($novo_jogador->save()){
+            return redirect()->route('home');
+        }
+        else{
+            echo "!ok";
+        }
+
 
     }
 
 
 
     public function time(){
-            $id = 1;
+            $id = 5;
             $times = Time::all();  //transformo a variavel em um conceito de time
             $time = $times->find($id); ////encontro o time que eu quero, contando que esteja no banco
-            echo $time->nome_time; //Aqui diz o nome do time
-            echo "<hr>";
-            
 
-            //Dados de jogadores desse time            
+
+            $alltimes = Time::select()->get();
+            echo "<hr>";
+            echo "<b>Times: </b>";
+            foreach($alltimes as $todos_times){
+                echo $todos_times->nome_time . " | ";
+            }
+
+
+            echo "<hr>";
+            echo  "<b>Time selecionado:</b> " . $time->nome_time; //Aqui diz o nome do time
+            echo "<hr>";
+
+            //===================================================================
+
+
+            //Dados de jogadores desse time
             $elenco = $time->where('time_id', $id); //Com o id do time, onde na tabela "Time" com o time_id = $id
             $jogadores = $elenco->first()->jogadordoTime; //traga dados
-
-
             foreach($jogadores as $jogador){
-            echo $jogador->nome_jogador."  ";
-            echo $jogador->posicao. "   ";
-            echo $jogador->numero."<br>";
+                echo $jogador->nome_jogador."  ";
+                echo $jogador->posicao. "   ";
+                echo $jogador->numero."<br>";
             }
         }
 
-    /* public function jogador(){
-        
-    
 
-    } */
+
+
 }
